c4d2cdf4815774dc94dc72d83540d02f366b6889
so1yana <trapbigest@gmail.com>
2024-07-22 19:02:50 +0300

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css';
+import {
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+  createBrowserRouter,
+} from 'react-router-dom';
+
+import Layout from './pages/layout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<h1>Main</h1>} />
+      <Route path="/store" element={<h1>Store</h1>} />
+      <Route path="/store/:id" element={<h1>Store item</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Мурманские салюты</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
