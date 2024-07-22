4efbbbe2d90c87a55bf35ee18a09445b7346f45b
Vasily S <rureeru@gmail.com>
2024-07-22 16:43:31 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 
@@ -7,7 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
