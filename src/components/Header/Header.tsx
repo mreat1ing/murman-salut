9968dd448ddede8f7e6ec76ad2b52b79dadb8420
so1yana <trapbigest@gmail.com>
2024-07-25 01:33:02 +0300

    reworked logic for display/hide header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,39 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import HeaderButton from 'src/ui/header-button';
+import useDirection from 'src/hooks/useDirection/useDirection';
 
 import logo from '../../assets/img/logo.webp';
 import './Header.scss';
 import Cart from '../Cart';
 
 const Header: FC = () => {
+  const headerContainer = useRef(document.querySelector('.header'));
+  const [isExpanded, setExpanded] = useState(false);
+  const [scrollDirection, scrollPosition] = useDirection();
+
+  useEffect(() => {
+    headerContainer.current = document.querySelector('.header');
+  }, []);
+
+  useEffect(() => {
+    if (scrollDirection === 'up') {
+      headerContainer.current?.classList.add('header--visible');
+    } else if (scrollDirection === 'down') {
+      headerContainer.current?.classList.remove('header--visible');
+    }
+    if (Number(scrollPosition) < 10) setExpanded(false);
+    else if (Number(scrollPosition) > 0 && scrollDirection === 'up')
+      setExpanded(true);
+    else if (scrollDirection === 'down') setExpanded(false);
+  }, [scrollDirection, scrollPosition]);
+
   return (
-    <header className="header">
-      <div className="header__gradient" />
+    <header className="header header--visible">
+      <div
+        className={`header__gradient ${isExpanded ? 'header__gradient--visible' : ''}`}
+      />
       <div className="header__content">
         <img className="header__image" src={logo} alt="logo" />
         <nav className="header__navigation">
