524f80fbee5e6582867312a21d7a5ad80aeb5284
so1yana <trapbigest@gmail.com>
2024-07-24 19:58:49 +0300

    some features

--- src/ui/header-button/HeaderButton.tsx
+++ src/ui/header-button/HeaderButton.tsx
@@ -1,13 +1,27 @@
-import { FC } from 'react';
+import { FC, ReactElement, ReactEventHandler } from 'react';
 
 import './HeaderButton.scss';
 
 interface IHeaderButton {
-  children: string;
+  children?: ReactElement | string;
+  onMouseOver?: ReactEventHandler;
+  onMouseOut?: ReactEventHandler;
 }
 
-const HeaderButton: FC<IHeaderButton> = ({ children }) => {
-  return <button className="header-button">{children}</button>;
+const HeaderButton: FC<IHeaderButton> = ({
+  children,
+  onMouseOut,
+  onMouseOver,
+}) => {
+  return (
+    <button
+      className="header-button"
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default HeaderButton;
