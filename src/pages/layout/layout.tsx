79d58a647819bc5589cf133663b73a567d668371
so1yana <trapbigest@gmail.com>
2024-07-25 18:35:12 +0300

    added image import

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from 'react-router-dom';
 
 import Header from 'src/components/Header';
-import Footer from 'src/components/Footer';
+// import Footer from 'src/components/Footer';
+import BackgroundImage from 'src/components/BackgroundImage';
+import image from 'src/assets/img/firework-light-blue.jpg';
 
 import './layout.scss';
 
@@ -12,6 +14,7 @@ export default function Layout() {
         <Header />
       </div>
       <div className="middle-container" style={{ height: 10000 }}>
+        <BackgroundImage imageSrc={image} alt="Салют" />
         <Outlet />
       </div>
       {/* <div className="footer-container">
