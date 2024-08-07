6fd9ad6ad0191b2cc76501902bdf87ce59819fc9
so1yana <trapbigest@gmail.com>
2024-08-07 19:10:47 +0300

    changed logo & moved wrapper from layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import { FC, useEffect, useRef, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import HeaderButton from 'src/ui/header-button';
 import useDirection from 'src/hooks/useDirection/useDirection';
 
-import logo from '../../assets/img/logo.webp';
+import logo from '../../assets/img/logo.png';
 import './Header.scss';
 import Cart from '../Cart';
 
@@ -30,26 +30,30 @@ const Header: FC = () => {
   }, [scrollDirection, scrollPosition]);
 
   return (
-    <header className="header header--visible">
-      <div
-        className={`header__gradient ${isExpanded ? 'header__gradient--visible' : ''}`}
-      />
-      <div className="header__content">
-        <img className="header__image" src={logo} alt="logo" />
-        <nav className="header__navigation">
-          <NavLink to="/">
-            <HeaderButton>Главная</HeaderButton>
-          </NavLink>
-          <NavLink to="/store">
-            <HeaderButton>Магазин</HeaderButton>
-          </NavLink>
-          <NavLink to="/about">
-            <HeaderButton>О нас</HeaderButton>
-          </NavLink>
-          <Cart />
-        </nav>
-      </div>
-    </header>
+    <div className="header-container">
+      <header className="header header--visible">
+        <div
+          className={`header__gradient ${isExpanded ? 'header__gradient--visible' : ''}`}
+        />
+        <div className="header__content">
+          <Link to="/">
+            <img className="header__image" src={logo} alt="logo" />
+          </Link>
+          <nav className="header__navigation">
+            <NavLink to="/">
+              <HeaderButton>Главная</HeaderButton>
+            </NavLink>
+            <NavLink to="/store">
+              <HeaderButton>Магазин</HeaderButton>
+            </NavLink>
+            <NavLink to="/about">
+              <HeaderButton>О нас</HeaderButton>
+            </NavLink>
+            <Cart />
+          </nav>
+        </div>
+      </header>
+    </div>
   );
 };
 export default Header;
