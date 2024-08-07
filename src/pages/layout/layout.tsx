7216db11a3c44e51f87faaed91c995ad8fa5b5c3
so1yana <trapbigest@gmail.com>
2024-08-07 19:10:58 +0300

    removed useless lines

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,25 +1,17 @@
 import { Outlet } from 'react-router-dom';
 
 import Header from 'src/components/Header';
-// import Footer from 'src/components/Footer';
-import BackgroundImage from 'src/components/BackgroundImage';
-import image from 'src/assets/img/firework-light-blue.jpg';
-
-import './layout.scss';
+import Footer from 'src/components/Footer';
+import Main from 'src/components/Main';
 
 export default function Layout() {
   return (
-    <div className="app-container">
-      <div className="header-container">
-        <Header />
-      </div>
-      <div className="middle-container" style={{ height: 10000 }}>
-        <BackgroundImage imageSrc={image} alt="Салют" />
+    <>
+      <Header />
+      <Main>
         <Outlet />
-      </div>
-      <div className="footer-container">
-        <Footer />
-      </div>
-    </div>
+      </Main>
+      <Footer />
+    </>
   );
 }
