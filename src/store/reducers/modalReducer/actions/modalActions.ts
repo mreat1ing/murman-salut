2882c57c3e13c437a00ea362e853529dd8964e71
so1yana <trapbigest@gmail.com>
2024-08-10 00:35:38 +0300

    split actions

--- src/store/reducers/modalReducer/actions/modalActions.ts
+++ src/store/reducers/modalReducer/actions/modalActions.ts
@@ -4,6 +4,10 @@ export const setModalOpen = () => {
   return { type: SET_CLOSE_OPEN, payload: true };
 };
 
+export const setModalClose = () => {
+  return { type: SET_CLOSE_OPEN, payload: false };
+};
+
 export const setModalType = (modalType: string) => {
   return { type: SET_TYPE, payload: modalType };
 };
