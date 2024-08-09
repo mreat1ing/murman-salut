efaa05efce6211853da8e5a96a784b1cbad86c5e
Vasily S <rureeru@gmail.com>
2024-08-09 18:32:24 +0300

    add about page and fix Main styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 
 import Layout from './pages/layout';
 import Store from './pages/store';
+import About from './pages/about';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,7 +15,7 @@ const router = createBrowserRouter(
       <Route path="/" element={<h1>Main</h1>} />
       <Route path="/store" element={<Store />} />
       <Route path="/store/:id" element={<h1>Store item</h1>} />
-      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/about" element={<About />} />
       <Route path="/cart" element={<h1>Корзина</h1>} />
     </Route>
   )
---
+++ src/pages/about/about.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import AboutUs from 'src/components/AboutUs';
+
+
+const About: FC = () => {
+    return (
+        <AboutUs />
+    );
+  };
+  
+  export default About;
+  
