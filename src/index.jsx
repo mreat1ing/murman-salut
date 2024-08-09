6ff271125196b035ccb02ec44b25b7eaf6adf11a
so1yana <trapbigest@gmail.com>
2024-08-09 23:09:28 +0300

    added store provider for entire app

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from 'react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import * as ReactDOM from 'react-dom/client';
 
 import './index.css';
 import './normalize.css';
 import App from './App';
+import { modalStore } from './store/reducers/modalReducer';
+
+const store = configureStore({ reducer: modalStore });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <div className="app-container">
-      <App />
-    </div>
+    <Provider store={store}>
+      <div className="app-container">
+        <App />
+      </div>
+    </Provider>
   </StrictMode>
 );
