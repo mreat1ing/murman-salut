87fdf96045c23f142ff44dc01e3cf1e37d4c6a59
so1yana <trapbigest@gmail.com>
2024-08-11 22:35:48 +0300

    added temp button & moved items to redux

--- src/components/Store/ItemList/ItemList.tsx
+++ src/components/Store/ItemList/ItemList.tsx
@@ -1,59 +1,26 @@
 import { FC, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import { IStoreItem } from 'src/interfaces/storeItem.interface';
+import { IStore } from 'src/interfaces/store.interface';
 import StoreItem from 'src/components/Store/StoreItem';
+import { fullStorageClear } from 'src/utils/localStore.utils';
 
 import './ItemList.scss';
-
-const tempItems: IStoreItem[] = [
-  {
-    _id: 'РС1705',
-    category: 'Бенгальские свечи',
-    hide: false,
-    link: 'https://youtu.be/n9cwvZqSq2Q',
-    price: 55,
-    title: 'РС1705 Бенгальский огонь-170',
-    value: 10,
-  },
-  {
-    _id: 'РС1730',
-    category: 'Бенгальские свечи',
-    hide: false,
-    link: 'https://youtu.be/gVy3SthKDnw',
-    price: 160,
-    title: 'РС1730 Бенгальский огонь 300 УЛЬТРА',
-    value: 4,
-  },
-  {
-    _id: 'РС1735',
-    category: 'Бенгальские свечи',
-    hide: false,
-    link: 'https://youtu.be/DOpDWJtbewk',
-    price: 275,
-    title: 'РС1735 Бенгальский огонь 450 УЛЬТРА',
-    value: 4,
-  },
-  {
-    _id: 'Р4810',
-    category: 'Бенгальские свечи',
-    hide: false,
-    link: 'https://orfvideo.synology.me/20_21/P4810.mp4',
-    price: 305,
-    title: 'Р4810 фонтан настольный',
-    value: 4,
-  },
-];
 
 const ItemList: FC = () => {
   const [items, setItems] = useState<IStoreItem[] | null>(null);
   const [isLoading, setLoading] = useState(false);
+  const itemsList = useSelector(
+    (state: IStore) => state.storeItemsReducer.items
+  );
 
   useEffect(() => {
     setLoading(true);
     const timer = setTimeout(() => {
-      setItems([...tempItems]);
+      setItems([...itemsList]);
       setLoading(false);
-    }, 3300);
+    }, 200);
 
     return () => clearTimeout(timer);
   }, []);
@@ -63,6 +30,7 @@ const ItemList: FC = () => {
 
   return (
     <ul className="store__items">
+      <button onClick={fullStorageClear}>Clear local storage</button>
       {items &&
         items.map((el) => {
           return <StoreItem key={el._id}>{el}</StoreItem>;
