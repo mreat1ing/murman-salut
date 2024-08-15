2ef55e2c8a40cec1bca6bd44943497a2e29eaf86
Vasily S <rureeru@gmail.com>
2024-08-15 22:30:58 +0300

    added cartItem interface

---
+++ src/interfaces/cartItem.interface.ts
@@ -0,0 +1,10 @@
+export interface ICartItem {
+    _id: string;
+    category: string;
+    hide: boolean;
+    link: string;
+    price: number;
+    title: string;
+    value: number;
+    count: number;
+  }
