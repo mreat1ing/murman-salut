d4729f0f4ae411c9f7d248d62095265989d1a238
Vasily S <rureeru@gmail.com>
2024-08-20 14:31:01 +0300

    updated header cart comp

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,21 +1,34 @@
-import { FC, useRef, useState, memo, useEffect } from 'react';
+import { FC, useRef, useState, memo } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import HeaderButton from 'src/ui/header-button';
 import { IStore } from 'src/interfaces/store.interface';
+import { ICartItem } from 'src/interfaces/cartItem.interface';
 
 import './Cart.scss';
+import HeaderCartItem from './HeaderCartItem';
 
 const Cart: FC = () => {
   const timer = useRef<NodeJS.Timeout>();
   const [isCartHover, setCartHover] = useState<boolean>(false);
+  const items = useSelector((state: IStore) => state.storeItemsReducer.cartItems);
+  const location = window.location.pathname === '/cart';
   const cartAmount = useSelector(
     (state: IStore) => state.storeItemsReducer.amountCart
   );
+  const formattedPrice = Intl.NumberFormat('RU-ru', {
+    style: 'currency',
+    currency: 'RUB',
+  }).format(Number(items.reduce((acc, element) => acc + element.count * element.price, 0)));
+  function pluralize(num: number, titles: string[]) {
+    const suffix = titles[num % 10 === 1 && num % 100 !== 11 ? 0 : num % 10 >= 2 && num % 10 <= 4 && (num % 100 < 10 || num % 100 >= 20) ? 1 : 2];
+    return suffix;
+  }
 
   const hoverHandler = (value: boolean) => {
-    if (cartAmount < 1) return;
+    if (items.length < 1) setCartHover(false);
+    if (items.length < 1) return;
     clearTimeout(timer.current);
     if (!value) {
       document.querySelector('.header__cart-list')?.classList.add('hide');
@@ -23,6 +36,12 @@ const Cart: FC = () => {
       document.querySelector('.header__cart-list')?.classList.remove('hide');
     timer.current = setTimeout(() => setCartHover(value), 500);
   };
+  const removeItemHandler = (value: boolean) => {
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => setCartHover(value), 500);
+    document.querySelector('.header__cart-list')?.classList.remove('hide');
+  };
+
   return (
     <>
       <NavLink to="/cart">
@@ -35,22 +54,25 @@ const Cart: FC = () => {
           <span className="cart__count">{cartAmount || 0}</span>
         </div>
       </NavLink>
-      {isCartHover && (
-        <div
+      {isCartHover &&  ( !(items.length < 1) &&
+        (!location && <div
           className="header__cart-list"
           onMouseOver={() => hoverHandler(true)}
           onMouseOut={() => hoverHandler(false)}
         >
-          <ul>
-            <li>Что-то</li>
-            <li>Что-то</li>
-            <li>Что-то</li>
-            <li>Что-то</li>
-            <li>Что-то</li>
-            <li>Что-то</li>
-          </ul>
+          {cartAmount && items ? 
+        <ul className='header__cart-list-items'>
+          {items.map((item: ICartItem) => {
+            return <HeaderCartItem key={item.title} setActive={removeItemHandler}>{item}</HeaderCartItem>;
+          })}
+        </ul> : <p className='header__cart-list-null'>В корзине пока нет товаров</p>}
+        <div className="header__cart__sum">
+          <div className="cart-list__sum-amount">Всего: {cartAmount + ''} {pluralize(Number(cartAmount), ['упаковка', 'упаковки', 'упаковок'])}</div>
+          <div className="cart-list__sum-price">Итого: {formattedPrice} </div>
         </div>
+        </div>)
       )}
+      
     </>
   );
 };
