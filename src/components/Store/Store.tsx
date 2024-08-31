c1cf190249ed128b570f1a63f2b02f17b2a5eb81
Vasily S <rureeru@gmail.com>
2024-08-31 16:26:49 +0300

    added store navigation and filters

--- src/components/Store/Store.tsx
+++ src/components/Store/Store.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useEffect, useState } from 'react';
+import { FC, memo, useCallback, useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -7,6 +7,8 @@ import CategorySkeleton from 'src/common/categorySkeleton';
 import useDispatchedStoreActions from 'src/hooks/useDispatchedStoreActions/useDispatchedStoreActions';
 import { db } from 'src/services/db';
 import { IStore } from 'src/interfaces/store.interface';
+import { iconFilter } from 'src/ui/icons/categoryIcons/CategoryIcons';
+import { IStoreItem } from 'src/interfaces/storeItem.interface';
 
 import ItemList from './ItemList';
 
@@ -15,6 +17,7 @@ import './Store.scss';
 const Store: FC = () => {
   const [categoriesList, setCategoriesList] = useState<React.ReactElement[]>();
   const { setItems, setItemsLoading } = useDispatchedStoreActions();
+  const [sort, setSort] = useState('Сортировка');
   const categories = useSelector(
     (state: IStore) => state.storeItemsReducer.categories
   );
@@ -28,6 +31,11 @@ const Store: FC = () => {
   const [searchParams, setSearchParams] = useSearchParams(
     `category=${curCategory}`
   );
+  const [listItems, setListItems] = useState(items);
+
+  useEffect(() => {
+    setSort('Сортировка');
+  }, [searchParams]);
 
   useEffect(() => {
     if (!categories.length) return;
@@ -39,6 +47,7 @@ const Store: FC = () => {
               onClick={() => setSearchParams(`category=${el.title}`)}
               title={el.title}
               value={el.title}
+              image={iconFilter(el.title)}
             />
           </li>
         );
@@ -55,6 +64,45 @@ const Store: FC = () => {
       setItemsLoading(false);
     });
   }, [items.length, setItems, setItemsLoading]);
+  
+  const filterItems = useCallback(() => {
+    let list: IStoreItem[] = [];
+    const copyItems = [...items];
+    if (searchParams.get('category') === 'Все') return setListItems(copyItems);
+    else {
+      const cat = categories.find(
+        (el) => el.title === searchParams.get('category')
+      );
+      if (cat?.subcategories) {
+        for (let i = 0; i < cat.subcategories.length; i++) {
+          list = list.concat(
+            items.filter((item) => item.category === cat.subcategories[i].value)
+          );
+        }
+      } else {
+        list = list.concat(
+          items.filter((item) => item.category === cat?.value)
+        );
+      }
+    }
+    setListItems(list);
+  }, [items, searchParams, categories]);
+  
+  useEffect(() => {
+    if (items.length) {
+      filterItems();
+    }
+  }, [filterItems, items]);
+
+  const sortItems = (val: string) => {
+    setSort(val);
+    const copyItems = listItems;
+    if (val === 'min') {
+      setListItems(copyItems.sort((a, b) => a.price - b.price));
+    } else if (val === 'max') {
+      setListItems(copyItems.sort((a, b) => a.price - b.price).reverse());
+    }
+  };
 
   return (
     <div className="store-page">
@@ -66,16 +114,16 @@ const Store: FC = () => {
         <div className="store">
           <div className="store__header">
             <h2 className="store__title">{searchParams.get('category')}</h2>
-            <select defaultValue="Сортировка">
+            <select onChange={(e) => sortItems(e.target.value)} value={sort}>
               <option value="Сортировка" disabled hidden>
                 Сортировка
               </option>
-              <option>Сначала недорогие</option>
-              <option>Сначала дорогие</option>
+              <option value={'min'}>Сначала недорогие</option>
+              <option value={'max'}>Сначала дорогие</option>
             </select>
           </div>
 
-          <ItemList />
+          {listItems && items && sort && <ItemList items={listItems} />}
         </div>
       ) : null}
     </div>
