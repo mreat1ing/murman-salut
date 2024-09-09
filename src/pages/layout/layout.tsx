d7bbd52b38f1cc3bffd45ebe87019f948538bece
Vasily S <rureeru@gmail.com>
2024-09-09 17:53:10 +0300

    added toTopButton and refactor styles

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 import Main from 'src/components/Main';
+import ScrollToTopButton from 'src/common/ScrollToTopButton';
 
 export default function Layout() {
   return (
@@ -11,6 +12,7 @@ export default function Layout() {
       <Main>
         <Outlet />
       </Main>
+      <ScrollToTopButton />
       <Footer />
     </>
   );
