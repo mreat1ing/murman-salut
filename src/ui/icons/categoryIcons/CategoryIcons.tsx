3c03dda8fc9e0eda55c10fc727e60f854c6f8da7
Vasily S <rureeru@gmail.com>
2024-09-16 19:00:34 +0300

    added new category icons

--- src/ui/icons/categoryIcons/CategoryIcons.tsx
+++ src/ui/icons/categoryIcons/CategoryIcons.tsx
@@ -9,15 +9,29 @@ import roket from 'src/assets/icons/categoryIcons/ico_roket.svg';
 import smoke from 'src/assets/icons/categoryIcons/ico_smoke.svg';
 import rome from 'src/assets/icons/categoryIcons/ico_rome.svg';
 import torch from 'src/assets/icons/categoryIcons/ico_torch.svg';
+import shari from 'src/assets/icons/categoryIcons/fest-shar2.png';
+import pnevmo from 'src/assets/icons/categoryIcons/iso_pnevmo.svg';
+import serpantin from 'src/assets/icons/categoryIcons/iso_serpantin.svg';
+import salut from 'src/assets/icons/categoryIcons/iso_soloSalut.svg';
+import nofly from 'src/assets/icons/categoryIcons/iso_nofly.svg';
+import daySalut from 'src/assets/icons/categoryIcons/iso_daySalut.svg';
 
 const iconFilter = (title: string) => {
   switch (title) {
     case 'Хлопушки':
       return hlop;
+    case 'Пневмохлопушки':
+      return pnevmo;
+    case 'Серпантин':
+      return serpantin;
     case 'Салюты':
       return rome;
-    case 'Вертушки':
+    case 'Одиночные салюты':
+      return salut;
+    case 'Вертушки летающие':
       return fly;
+    case 'Вертушки наземные':
+      return nofly;
     case 'Бенгальские свечи':
       return bengal;
     case 'Петарды':
@@ -34,6 +48,8 @@ const iconFilter = (title: string) => {
       return torch;
     case 'Батареи салютов':
       return batary;
+    case 'Дневные салюты':
+      return daySalut;
     default:
       return null;
   }
