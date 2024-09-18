091b71e925e97b56d79dcfa1053815a43091421a
Vasily S <rureeru@gmail.com>
2024-09-18 16:02:41 +0300

    refactored 404 page +  fixed text

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -16,8 +16,8 @@ const NotFound: FC = () => {
       <h1 className='not-found__title'>404</h1>
       <div className="not-found__text-container">
         <h3 className='not-found__text-title'>Такой страницы у нас нет.<br/> она была. а теперь нет.</h3>
-        <p className='not-found__text'>Но это не трашно! Вы по прежнему можетие найти нужные товары в нашем каталоге</p>
-        <Link draggable={false} to={'/store'}><span className='not-found__text-link'>перейти в каталог товаров</span></Link>
+        <p className='not-found__text'>Но это не страшно! Вы по прежнему можетие найти нужные товары в нашем каталоге</p>
+        <Link draggable={false} to={'/store'}><span className='not-found__text-link'>Перейти в каталог товаров</span></Link>
       </div>
       
       
